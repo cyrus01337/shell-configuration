25855b4e3d99564ede0808a743ca7f9f67b9649b
cyrus01337 <cyrus21337@gmail.com>
2024-10-30 17:18:16 +0000

    Remove Chezmoi module as no longer used

--- conf.d/modules/chezmoi.fish
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env fish
-set -x AUTO_UPDATE_CHEZMOI_SERVICE "$HOME/.config/systemd/user/chezmoi.service"
-
-if which chezmoi &> /dev/null
-and ! systemctl --user is-enabled --quiet chezmoi.service
-and [ -f $AUTO_UPDATE_CHEZMOI_SERVICE ]
-and [ ! -f "/.dockerenv" ]
-    echo "chezmoi auto-update service found, don't forget to enable it with \"systemctl --user enable --now chezmoi.service\""
-end
