841907260379e860324a910c4ab5b776ac27ff18
cyrus01337 <cyrus01337@gmail.com>
2024-11-05 07:16:44 +0000

    Simplify by using dynamic aliases

--- conf.d/packages/loaders/javascript.fish
+++ conf.d/packages/loaders/javascript.fish
@@ -1,29 +1,32 @@
 #!/usr/bin/env fish
-set P_PACKAGE_MANAGER ""
-set PX_COMMAND ""
+function p_detect
+    if begin
+        [ -f "package-lock.json" ]
+        or [ -f "yarn.lock" ]
+        or [ -f "pnpm-lock.yaml" ]
+        or [ -f "deno.lock" ]
+        or [ -f "bun.lockb" ]
+    end
+        return 0
+    end
 
-function detect_javascript
+    return 127
+end
+
+function p_setup
     if [ -f "package-lock.json" ]
-        set P_PACKAGE_MANAGER "npm"
-        set PX_COMMAND "npx"
-    else if [ -f "yarn.lock" ]
-        set P_PACKAGE_MANAGER "yarn"
-        set PX_COMMAND "yarn dlx"
-    else if [ -f "pnpm-lock.yaml" ]
-        set P_PACKAGE_MANAGER "pnpm"
-        set PX_COMMAND "pnpx"
-    else if [ -f "deno.lock" ]
-        set P_PACKAGE_MANAGER "deno"
-        set PX_COMMAND "deno run"
-    else if [ -f "bun.lockb" ]
-        set P_PACKAGE_MANAGER "bun"
-        set PX_COMMAND "bunx"
+        alias p "npm"
+        alias pi "npm install"
+        alias px "npx"
     else
         return 127
     end
 
-    echo $P_PACKAGE_MANAGER
-    echo $PX_COMMAND
+    return 0
+end
+
+function p_teardown
+    functions --erase p pi px
 
     return 0
 end
